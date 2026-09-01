14c9b58d24bc0ad7cbf85b19b13d30986ef700be
Tushar3888 <TUSHAR.30008@stu.upes.ac.in>
2026-09-01 21:48:09 +0530

    Add library fine calculation program

---
+++ DAY12-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int lateDays, fine = 0;
+printf("Enter the number of late days: ");
+    scanf("%d", &lateDays);
+    if (lateDays <= 0) 
+    {
+        printf("No fine.\n");
+    }
+    else if (lateDays <= 5)
+    {
+        fine = lateDays * 2;
+        printf("Library Fine = ₹%d\n", fine);
+    }
+    else if (lateDays <= 10) 
+    {
+        fine = (5 * 2) + (lateDays - 5) * 4;
+        printf("Library Fine = ₹%d\n", fine);
+    }
+    else if (lateDays <= 30) 
+    {
+        fine = (5 * 2) + (5 * 4) + (lateDays - 10) * 6;
+        printf("Library Fine = ₹%d\n", fine);
+    }
+    else 
+    {
+        printf("Membership cancelled due to excessive late returns.\n");
+    }
+    return 0;
+}
